ed9e9891815736e611da0394e5d27239e0e1f5b5
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-12-01 23:56:34 +0300

    fixing email sending progress 3

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,15 +4,10 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
    
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465, 
-        secure: true,
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER, 
             pass: process.env.EMAIL_PASS  
-        },
-        tls: {
-            rejectUnauthorized: false 
         }
     });
 
